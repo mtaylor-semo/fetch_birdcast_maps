8420042efe6d2bff50da530c760ef5393ec06230
Michael S. Taylor <mtaylor@semo.edu>
2026-06-06 08:56:24 -0500

    Deleted unneeded info. Modified to spring migration.

--- fetch_birdcast.R
+++ fetch_birdcast.R
@@ -21,101 +21,78 @@ library(suncalc)
 
 # Constants ---------------------------------------------------------------
 
-# Constants
-# Miller is GMT-6, but see https://en.wikipedia.org/wiki/Tz_database#Area
-# for explanation why the sign is inverted for Etc codes.
-time_zone <- "Etc/GMT+6"
-
-# GMT+7 should equate to two hours after sunset for Miller Reserve.
-# For example, 18:00 CST is 19:00 EST, plus two more hours for delay
-# after sunset.
-
-map_time_zone <- "Etc/GMT+7"
-
-# First attempt at getting sunrise and sunset times. Note that
-# 'Etc/GMT+6' is Olson Code for central standard time. The recorders
-# will be kept in standard time, never daylight savings time.
-# fred <- getSunlightTimes(
-#   date = as.Date("2026-03-11"), 
-#   lat = 37.132025, 
-#   lon = -89.461307,
-#   tz = tzone, 
-#   keep = c("sunrise", "sunset"))
-
-
-# Create a tibble with date, sunrise, and sunset for Miller Reserve.
-# NOTE: The times for New Madrid (Prairie) recorder are probably close enough
-# but determine if need a separate set for that recorder in the winter.
-
 # Modified from https://stackoverflow.com/a/68550338/3832941
-start_date = "20260315"
-end_date = "20260531"
-Dates <- seq(ymd(start_date),ymd(end_date), by = "days")
 latitude = 37.132025
 longitude = -89.461307
 
+spring_start_date = "20260315"
+spring_end_date = "20260531"
+spring_dates <- seq(ymd(spring_start_date),ymd(spring_end_date), by = "days")
 
-sunsets <- getSunlightTimes(Dates,latitude,longitude,tz = "GMT")$sunset
+# DECIDE HOW TO HANDLE FALL AND SPRING
+
+
+# Images are stored by GMT, but show EST on the image.
+spring_sunsets <- getSunlightTimes(spring_dates,latitude,longitude,tz = "GMT")$sunset
 
 # Add two hours to GMT to get two hours after sunset, using lubridate
-sunsets_plus_two <- sunsets + hours(2)
+spring_sunsets_plus_two <- spring_sunsets + hours(2)
 
 # Round to nearest 10 minutes
-sunsets_plus_two <- round_date(sunsets_plus_two, "10 minutes")
+spring_sunsets_plus_two <- round_date(spring_sunsets_plus_two, "10 minutes")
 
-sunset_length <- length(sunsets_plus_two)
-
+sunset_length <- length(spring_sunsets_plus_two)
+sunset_length <- length(fall_sunsets_plus_two)
 # Alternative
 # sunsets_plus_two <- sunsets_plus_two <- sunsets + 2*60*60
 
 # Birdcast seems to put the night's images in the next day's date.
 # e.g., The night of 202605 is stored in 202606
-
-https://is-birdcast-observed-prod.s3.us-east-1.amazonaws.com/mosaic/2026/03/17/mosaic_202603170210.jpg
+# https://is-birdcast-observed-prod.s3.us-east-1.amazonaws.com/mosaic/2026/03/17/mosaic_202603170210.jpg
 
 base_url <- "https://is-birdcast-observed-prod.s3.us-east-1.amazonaws.com/mosaic/"
 
 url_frag <- paste0(
-  year(sunsets_plus_two),
+  year(spring_sunsets_plus_two),
   "/0",
-  month(sunsets_plus_two),
+  month(spring_sunsets_plus_two),
   "/"
 )
 
 for (i in 1:sunset_length) {
-  if (day(sunsets_plus_two[i]) < 10)
-    url_frag[i] = paste0(url_frag[i], "0", day(sunsets_plus_two[i]), "/")
+  if (day(spring_sunsets_plus_two[i]) < 10)
+    url_frag[i] = paste0(url_frag[i], "0", day(spring_sunsets_plus_two[i]), "/")
   else
-    url_frag[i] = paste0(url_frag[i], day(sunsets_plus_two[i]), "/")
+    url_frag[i] = paste0(url_frag[i], day(spring_sunsets_plus_two[i]), "/")
 }
 
 
 
 image_name <- paste0(
   "mosaic_",
-  year(sunsets_plus_two),
-  "0", month(sunsets_plus_two))
+  year(spring_sunsets_plus_two),
+  "0", month(spring_sunsets_plus_two))
 
 for (i in 1:sunset_length) {
-  if (day(sunsets_plus_two[i]) < 10)
-    image_name[i] = paste0(image_name[i], "0", day(sunsets_plus_two[i]))
+  if (day(spring_sunsets_plus_two[i]) < 10)
+    image_name[i] = paste0(image_name[i], "0", day(spring_sunsets_plus_two[i]))
   else
-    image_name[i] = paste0(image_name[i], day(sunsets_plus_two[i]))
+    image_name[i] = paste0(image_name[i], day(spring_sunsets_plus_two[i]))
 }
 
 
 for (i in 1:sunset_length) {
-  if (length(hour(sunsets_plus_two[i])) == 1)
-      image_name[i] = paste0(image_name[i], "0", hour(sunsets_plus_two[i]))
+  if (length(hour(spring_sunsets_plus_two[i])) == 1)
+      image_name[i] = paste0(image_name[i], "0", hour(spring_sunsets_plus_two[i]))
   else
-    image_name[i] = paste0(image_name[i], hour(sunsets_plus_two[i]))
+    image_name[i] = paste0(image_name[i], hour(spring_sunsets_plus_two[i]))
 }
 
 for (i in 1:sunset_length) {
-  if (minute(sunsets_plus_two[i]) < 10)
-    image_name[i] = paste0(image_name[i], "0", minute(sunsets_plus_two[i]), ".jpg")
+  if (minute(spring_sunsets_plus_two[i]) < 10)
+    image_name[i] = paste0(image_name[i], "0", minute(spring_sunsets_plus_two[i]), ".jpg")
   else
-    image_name[i] = paste0(image_name[i], minute(sunsets_plus_two[i]), ".jpg")
+    image_name[i] = paste0(image_name[i], minute(spring_sunsets_plus_two[i]), ".jpg")
 }
 
 final_urls <- ""
